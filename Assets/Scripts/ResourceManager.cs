0d6d06a0d62c4d0732b092cf6aec198fbe48bdc6
Simeón Ruiz <sruiz1983@gmail.com>
2020-10-10 18:25:17 +0200

    Fix ResourceUpdate to use proper gaussian function values

--- Assets/Scripts/ResourceManager.cs
+++ Assets/Scripts/ResourceManager.cs
@@ -47,12 +47,16 @@ public class ResourceManager : MonoBehaviour
 
         var newWater = temperature * (-0.1) + water + nature * (0.1) + population * (-0.1);
 
-        var newNature = (GaussDistribution(temperature, 10, 100, 30) - 15) + water * (0.1) + nature + population * (-0.1);
-        Debug.Log("Gauss Distribution result for nature modification by temperature" + (GaussDistribution(temperature, 10, 100, 30) - 15));
+        var newNature = (GaussDistribution(temperature, 25, 50, 15) - 15) + water * (0.1) + nature + population * (-0.1);
 
-        var newPopulation = (GaussDistribution(temperature, 10, 100, 30) - 15) + (GaussDistribution(water, 10, 100, 45) - 15) + nature * (0.1) + population;
-        Debug.Log("Gauss Distribution result for population modification by temperature" + (GaussDistribution(temperature, 10, 100, 30) - 15));
-        Debug.Log("Gauss Distribution result for population modification by water" + (GaussDistribution(water, 10, 100, 45) - 15));
+        var newPopulation = (GaussDistribution(temperature, 25, 50, 15) - 15) + (GaussDistribution(water, 25, 50, 20) - 15) + nature * (0.1) + population;
+        Debug.Log("Gauss Distribution result for population modification by temperature" + (GaussDistribution(temperature, 25, 50, 15) - 15));
+        Debug.Log("Gauss Distribution result for population modification by water" + (GaussDistribution(water, 25, 50, 20) - 15));
+
+        Debug.Log("Temperature variation: " + (newTemperature - temperature));
+        Debug.Log("Water variation: " + (newWater - water));
+        Debug.Log("Nature variation: " + (newNature - nature));
+        Debug.Log("Population variation: " + (newPopulation - population));
 
         temperature = newTemperature;
         water = newWater;
@@ -61,7 +65,7 @@ public class ResourceManager : MonoBehaviour
         UpdateCanvas();
     }
 
-    double GaussDistribution(double x, double a, double b, double c)
+    private double GaussDistribution(double x, double a, double b, double c)  // f(x) = a * e^(-(b-100)^2/(2 * c^2)) where "a" controls the height, "b" controls the center and "c" the width 
     {
         var v1 = (x - b);
         var v2 = (v1 * v1) / (2 * (c * c));
@@ -80,13 +84,13 @@ public class ResourceManager : MonoBehaviour
 
 
     // Start is called before the first frame update
-    void Start()
+    private void Start()
     {
         UpdateCanvas();
     }
 
     // Update is called once per frame
-    void Update()
+    private void Update()
     {
 
     }
